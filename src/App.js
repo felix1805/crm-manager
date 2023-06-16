44948534572ca0b064846defbeaf5754ccdc2e04
felix1805 <felix1805@gmail.com>
2023-06-16 16:34:02 -0400

    created folder arrangement and file structure, began building Dashboard display

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import TicketPage from './pages/TicketPage';
+import Nav from './components/Nav';
+
+const App = () => {
+  return (
+    <div className="app">
+      <BrowserRouter>
+      <Nav/>
+        <Routes>
+          <Route
+            path='/'
+            element={<Dashboard />}
+          ></Route>
+          <Route
+            path='/ticket'
+            element={<TicketPage />}
+          ></Route>
+          <Route
+            path='/ticket/:id'
+            element={<TicketPage />}
+            editMode={true}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div >
+  )
+}
+
+export default App;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,52 @@
+import TicketCard from "../components/TicketCard";
+
+const Dashboard = () => {
+  const tickets = [
+   {
+    category: 'Q1 2023',
+    color: 'blue',
+    title: 'Study Progress so far',
+    owner: 'Felix Petzsche',
+    avatar: 'https://avatars.githubusercontent.com/u/1591804?v=4',
+    status: 'in Progress',
+    priority: 5,
+    progress: 40,
+    description: 'Keep track of coding study progress in 2023 so far.',
+    timestamp: '2023-06-16T16:27:45-0400'
+   }, 
+   {
+    category: 'Q2 2023',
+    color: 'purple',
+    title: 'React project Guide',
+    owner: 'Felix Petzsche',
+    avatar: 'https://avatars.githubusercontent.com/u/1591804?v=4',
+    status: 'need to start on it',
+    priority: 8,
+    progress: 0,
+    description: 'Create a react project guide for new apps to be developed.',
+    timestamp: '2023-06-19T13:32:14-0400'
+   }, 
+   {
+    category: 'Q1 2023',
+    color: 'blue',
+    title: 'Completed Projects this Year',
+    owner: 'Felix Petzsche',
+    avatar: 'https://avatars.githubusercontent.com/u/1591804?v=4',
+    status: 'in Progress',
+    priority: 3,
+    progress: 20,
+    description: 'Tracking record of completed projects this year so far',
+    timestamp: '2023-06-14T13:57:26-0400'
+   } 
+  ]
+  return (
+    <div className="dashboard">
+      <h1>My Projects</h1>
+      <div className="ticket-container">
+        <TicketCard/>
+      </div>
+    </div>
+  )
+};
+
+export default Dashboard
