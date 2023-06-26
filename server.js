14122d0e1b480f62d4a60143dfa1adac00d0c06b
felix1805 <felix1805@gmail.com>
2023-06-26 17:34:18 -0400

    started backend construction, setting up database

---
+++ server.js
@@ -0,0 +1,12 @@
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const axios = require('axios');
+
+const app = express();
+
+const url = 
+const token = 'AstraCS:RIicsNnNrxhrmxHEgnYihBJB:1f2320de1e4e68ef4abaa79bacaea7255d935c39adac506627c7f835e49e6d34'
+
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`))
